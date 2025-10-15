74443c77960101b1a0055b30f457cdc8384c6d62
LSM-1020 <smmlee@naver.com>
2025-10-15 16:10:37 +0900

    페이징 기능 추가

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -9,14 +9,21 @@ function Board({user}) {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
   const [error, setError]=useState(null);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
 
 
-  //게시판 모든글 요청
-  const loadPost = async()=>{
+  //게시판 페이징된 글 리스트 요청
+  const loadPost = async(page=0)=>{
     try{
       setLoading(true);
-      const res = await api.get("/api/board"); //모든글 가져오기 요청
-      setPosts(res.data); //posts->전체 게시글
+      const res = await api.get(`/api/board?page=${page}&size=10`); //모든글 가져오기 요청
+      setPosts(res.data.posts); //posts->전체 게시글
+      setCurrentPage(res.data.currentPage);//현제 페이지 번호
+      setTotalPages(res.data.totalPages);//전체 페이지수
+      setTotalItems(res.data.totalItems);//전체 게시글 갯수
+
     } catch(err){
         console.error(err);
         setError("게시글 조회 실패")
@@ -36,13 +43,25 @@ function Board({user}) {
   }
 
   useEffect(()=>{
-   loadPost();
-  },[])
+   loadPost(currentPage);
+  },[currentPage])
+
+  //페이지번호 그룹 배열 반환 함수(10개까지만 표시)
+  const getPageNumbers=()=>{
+    const startPage = (Math.floor(currentPage/10))*10; 
+    const endPage = (startPage+10) > totalPages ? totalPages : (startPage+10) ; //참이면 totalpage,거짓이면startpage+10
+    const pages = [];
+    for (let i = startPage; i < endPage; i++) {
+      pages.push(i);
+    }
+    return pages;
+  }
 
   //날짜 포멧 함수
   const formatDate =(dateString)=>{ //formatDate를 만들어서 dateString이라는 값을 넣어줌
     const date = new Date(dateString); //dateString은 Date타입으로 만들어주고 date라는 이름으로 선언
     return date.toLocaleDateString(); //리턴값은 date.Date타입이라 쓸수있는 toLocaldatestring으로
+    //return datdString.substring(0,10);
   }
 
   return (
@@ -87,6 +106,32 @@ function Board({user}) {
           )}
         </tbody>
       </table>
+      {/* 페이지 번호와 이동화살표 출력 */}
+      <div className="pagenation">
+        {/* 첫번째 페이지로 이동 */}
+        <button onClick={()=>setCurrentPage(0)}
+          disabled={currentPage===0}>
+          ◀◀
+          </button>
+        <button onClick={()=>setCurrentPage(currentPage-1)}
+          disabled={currentPage===0}>
+          ◀
+          </button>
+          {/* 페이지 번호 그룹 10개씩 출력 */}
+          {getPageNumbers().map((num)=>(
+            <button className={num === currentPage ? "active":""} onClick={()=>setCurrentPage(num)}>{num+1}</button>
+          ))}
+          
+        <button onClick={()=>setCurrentPage(currentPage+1)}
+          disabled={currentPage===(totalPages-1) || totalPages === 0}>
+          ▶
+        </button>
+        {/* 마지막 페이지로 이동 */}
+        <button onClick={()=>setCurrentPage(totalPages-1)}
+          disabled={currentPage===(totalPages-1) || totalPages === 0}>
+          ▶▶
+        </button>
+      </div>
       <div className="write-button-container">
         <button onClick={handleWrite} className="write-button">글쓰기</button>
       </div>
