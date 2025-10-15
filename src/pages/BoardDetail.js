c8ee0dcb6ddbe4a69faf334dabb171125077ee9a
LSM-1020 <smmlee@naver.com>
2025-10-15 11:49:03 +0900

    회원가입 유효성체크

--- src/pages/BoardDetail.js
+++ src/pages/BoardDetail.js
@@ -24,7 +24,7 @@ function BoardDetail({user}) {
     } catch(err) {
       console.error(err);
       setError("존재하지 않는 글입니다")
-      // alert("존재하지 않는 글입니다");
+      
     } finally {
       setLoading(false);
     }
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -3,33 +3,44 @@ import "./Signup.css";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import api from "../api/axiosConfig";
+import { type } from "@testing-library/user-event/dist/type";
 
 function Signup() {
     const[username, setUsername] = useState("");
     const[password, setPassword] = useState("");
+    const [errors, setErrors] = useState({});
     const navigate = useNavigate();
 
 
-    const handleSingup = async(e)=>{
+    const handleSignup = async(e)=>{
       e.preventDefault();
+      setErrors({});
       try {
         await api.post("/api/auth/signup",{username, password});
         alert("회원가입 성공")
         navigate("/login")
       }catch (err) {
-        console.error("회원가입 실패",err)
-        alert("회원가입 실패")
+        if(err.response && err.response.status===400){ //회원가입 400에러 발생
+          setErrors(err.response.data); //에러 추출,errors에 저장
+        } else {
+          console.error("회원가입 실패",err)
+          alert("회원가입 실패") 
+        }
+        
       }
     }
 
   return (
     <div className="form-container">
       <h2>회원가입</h2>
-      <form onSubmit={handleSingup}>
+      <form onSubmit={handleSignup}>
         <input type="text" placeholder="아이디" value={username}
             onChange={(e)=>setUsername(e.target.value)} />
+              {errors.username && <p style={{color:"red"}}>{errors.username}</p>}
+              {errors.iderror && <p style={{color:"red"}}>{errors.iderror}</p>}
         <input type="password" placeholder="비밀번호" value={password}
-            onChange={(e)=>setPassword(e.target.value)} />    
+            onChange={(e)=>setPassword(e.target.value)} />  
+              {errors.password && <p style={{color:"red"}}>{errors.password}</p>}  
          <button type="submit">회원가입</button>
       </form>
       
